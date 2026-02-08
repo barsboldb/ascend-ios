7fe3b0b9cd9d818d71783c95a8781ee29fed53b1
barsbold-coding <barsbold.coding@gmail.com>
2026-02-08 09:52:57 +0800

    Initial Commit

---
+++ Ascend/AscendApp.swift
@@ -0,0 +1,17 @@
+//
+//  AscendApp.swift
+//  Ascend
+//
+//  Created by Barsbold Bayarerdene on 08/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct AscendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AscendTests/AscendTests.swift
@@ -0,0 +1,17 @@
+//
+//  AscendTests.swift
+//  AscendTests
+//
+//  Created by Barsbold Bayarerdene on 08/02/2026.
+//
+
+import Testing
+@testable import Ascend
+
+struct AscendTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
